cb29f37b95b8364701bd2aa4b4ca2e353fbf805b
Michael Haberler <git@mah.priv.at>
2026-03-31 16:04:04 +0200

    build

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import L from 'leaflet';
 
 L.Icon.Default.imagePath = 'img/icon/';
 
-const m_mono = L.tileLayer('https://tile.mierune.co.jp/mierune_mono/{z}/{x}/{y}.png', {
-    attribution: "Maptiles by <a href='https://mierune.co.jp/' target='_blank'>MIERUNE</a>, under CC BY. Data by <a href='https://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors, under ODbL."
+const m_mono = L.tileLayer(
+    'https://tile.openstreetmap.de/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors',
 });
 
 const map = L.map('map', {
-    center: [35.681, 139.767],
-    zoom: 11,
+    center: [47,15],
+    zoom: 12,
     zoomControl: true,
     layers: [m_mono]
 });
